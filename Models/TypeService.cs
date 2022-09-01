66d8473e07e5b1e777896a628493288736da7567
ValeryShinigami <valerykalombo@hotmail.fr>
2022-09-01 18:15:45 +0200

    add display name dans enum

--- Models/TypeService.cs
+++ Models/TypeService.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
 namespace coproBox.Models
 {
     public enum TypeService // avec "enum", chaque élément sera associé à un entier...
     {
-        Baby_Sitting, Cagnotte_Solidaire, Courses, Don_Objet, Evènement, Garde_Animaux, Ménage, Parking, Prêt_Matériel, Soutien_Scolaire 
+        [Display(Name = "Baby Sitting")]
+        Baby_Sitting,
+        [Display(Name = "Cagnotte Solidaire")]
+        Cagnotte_Solidaire,
+        [Display(Name = "Courses")]
+        Courses,
+        [Display(Name = "Don D'objet")]
+        Don_Objet,
+        [Display(Name = "Evènement")]
+        Evènement,
+        [Display(Name = "Garde Animaux")]
+        Garde_Animaux,
+        [Display(Name = "Ménage")]
+        Ménage,
+        [Display(Name = "Parking")]
+        Parking,
+        [Display(Name = "Prêt Matériel")]
+        Prêt_Matériel,
+        [Display(Name = "Soutien Scolaire")]
+        Soutien_Scolaire 
     }
 }
 
